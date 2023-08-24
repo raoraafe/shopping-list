61d9a3b1f7810691f6964f60a280463571c18de8
Rafay Taher <rafaytaher@gmail.com>
2023-08-24 23:34:39 +0500

    shopping list remove, checked functionality and edit to be done

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 //autoload files
 spl_autoload_register(function ($class) {
-	$prefix = 'model\\';
-	$baseDir = __DIR__ . '/model/';
-	$len = strlen($prefix);
-	if (strncmp($prefix, $class, $len) !== 0) {
-		return; // The class does not belong to the 'model' namespace, so return
-	}
-	$relativeClass = substr($class, $len);
-	$file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
-	//echo $file;exit;
-	if (file_exists($file)) {
-		require $file;
-	}
+    $prefix = 'model\\';
+    $baseDir = __DIR__.'/model/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return; // The class does not belong to the 'model' namespace, so return
+    }
+    $relativeClass = substr($class, $len);
+    $file = $baseDir.str_replace('\\', '/', $relativeClass).'.php';
+    //echo $file;exit;
+    if (file_exists($file)) {
+        require $file;
+    }
 });
 
 
@@ -21,24 +21,73 @@ use model\ShoppingList;
 
 $shoppingList = new ShoppingList('localhost', 'root', '', 'shopping_list');
 
-// Add items
-$shoppingList->addItem('Milk');
-$shoppingList->addItem('Eggs');
-$shoppingList->addItem('Bread');
+// Handle adding a new item
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['newItem'])) {
+    $newItem = $_POST['newItem'];
+    if (!empty($newItem)) {
+        $shoppingList->addItem($newItem);
+    }
+}
 
-// Mark an item as checked
-$shoppingList->markItemAsChecked(2);
+// Handle removing an item
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['removeItem'])) {
+    $removeItemId = intval($_GET['removeItem']);
+    $shoppingList->deleteItem($removeItemId);
+    header("Location: index.php"); // Redirect to refresh the page
+    exit();
+}
 
-// Edit an item
-$shoppingList->editItem(2, 'Butter');
+// Handle update checked item(s)
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['checkedItems'])) {
+    $checkedItems = $_POST['checkedItems'];
+    foreach ($checkedItems as $checkedItem) {
 
-// Delete an item
-$shoppingList->deleteItem(1);
+        $shoppingList->markItemAsChecked($checkedItem);
+    }
+
+    header("Location: index.php"); // Redirect to refresh the page
+    exit();
+}
 
 // Get the updated shopping list
 $items = $shoppingList->getItems();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Shopping List</title>
+</head>
+<body>
+<h1>Shopping List</h1>
 
-// Display the shopping list
-foreach ($items as $index => $item) {
-	echo ($item->isChecked() ? '[X] ' : '[ ] ') . $item->getName() . PHP_EOL;
-}
+<!-- Add new item form -->
+<form method="POST">
+    <label>
+        <input type="text" name="newItem" placeholder="Enter new item">
+    </label>
+    <button type="submit">Add Item</button>
+</form>
+
+<?php if (!empty($items)): ?>
+
+    <!-- update for checked item form -->
+    <form method="POST">
+        <!-- Display the shopping list -->
+        <ul>
+            <?php foreach ($items as $item): ?>
+                <li>
+                    <label>
+                        <input type="checkbox" name="checkedItems[]" value="<?php echo $item->getId(); ?>"
+                            <?php echo $item->isChecked() ? 'checked' : ''; ?>>
+                        <?php echo $item->getName(); ?>
+                    </label>
+                    <a href="?removeItem=<?php echo $item->getId(); ?>">Remove</a> | <a
+                            href="?editItem=<?php echo $item->getId(); ?>">Edit</a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <button type="submit">Update Checked Items</button>
+    </form>
+<?php endif; ?>
+</body>
+</html>
--- model/Item.php
+++ model/Item.php
@@ -1,29 +1,41 @@
 <?php
 
 namespace model;
-class Item {
 
-	private string $name;
-	private bool $checked;
+class Item
+{
+    private int $itemId;
+    private string $name;
+    private bool $checked;
 
-	public function __construct( $name ) {
-		$this->name    = $name;
-		$this->checked = false;
-	}
+    public function __construct($id, $name)
+    {
+        $this->name = $name;
+        $this->checked = false;
+        $this->itemId = $id;
+    }
 
-	public function getName(): string {
-		return $this->name;
-	}
+    public function getId(): int
+    {
+        return $this->itemId;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
-	public function isChecked(): bool {
-		return $this->checked;
-	}
+    public function isChecked(): bool
+    {
+        return $this->checked;
+    }
 
-	public function checkItem(): void {
-		$this->checked = true;
-	}
+    public function checkItem(): void
+    {
+        $this->checked = true;
+    }
 
-	public function uncheckItem(): void {
-		$this->checked = false;
-	}
+    public function uncheckItem(): void
+    {
+        $this->checked = false;
+    }
 }
